3889aadfb7dee37351b6e17483320f61aae54376
Malcolm <malcolmbalch@gmail.com>
2019-05-16 20:54:56 -0400

    Make everything floast

--- mmm/src/main.rs
+++ mmm/src/main.rs
@@ -1,27 +1,27 @@
 use std::io;
 use std::io::prelude::*;
 
-fn mean(input: &Vec<i64>) -> i64 {
-    let mut sum = 0;
+fn mean(input: &Vec<f64>) -> f64 {
+    let mut sum = 0.0;
     for num in input.iter() {
        sum += num;
     }
-    return (sum / input.len() as i64) as i64;
+    return (sum / input.len() as f64) as f64;
 }
 
-fn median(input: &Vec<i64>) -> i64 {
+fn median(input: &Vec<f64>) -> f64 {
     let mut to_sort = input.clone();
-    to_sort.sort();
+    to_sort.sort_by(|a, b| a.partial_cmp(b).unwrap());
     return to_sort[to_sort.len() / 2];
 }
 
-fn min_max(input: &Vec<i64>) -> (i64, i64) {
+fn min_max(input: &Vec<f64>) -> (f64, f64) {
     let mut to_sort = input.clone();
-    to_sort.sort();
+    to_sort.sort_by(|a, b| a.partial_cmp(b).unwrap());
     return (to_sort[0], to_sort[to_sort.len()-1]);
 }
 
-fn sum(input: &Vec<i64>) -> i64 {
+fn sum(input: &Vec<f64>) -> f64 {
     input.iter().sum()
 }
 
@@ -31,7 +31,7 @@ fn main() -> io::Result<()> {
     let mut input = Vec::new();
     for line in stdin.lock().lines() {
         let line_input = line.expect("Unable to get stdin line");
-        let num = line_input.parse::<i64>().expect("Only i64 is currently supported");
+        let num = line_input.parse::<f64>().expect("Only f64 is currently supported");
         input.push(num);
     }
     let mean = mean(&input);
